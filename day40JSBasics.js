d2728ff5211d02594d080cd4a8f3bf91f71db6c7
Vinay <navik.vinay12@gmail.com>
2022-12-29 01:10:45 +0530

    use random to get output 1 to 6

--- day40JSBasics.js
+++ day40JSBasics.js
@@ -1,4 +1,7 @@
 //  JavaScript Basic Practice Problems  .
-
+/*
 //To Get a Single Digit Number .
 console.log(Math.floor((Math.random()*10)%10));
+*/
+//Use Random to get Dice Number between 1 to 6
+console.log(Math.floor((Math.random()*(6)+1)));
