fdc7938605f758c45e672c2530fd13331e03f7a3
Vinay <navik.vinay12@gmail.com>
2022-12-29 01:12:59 +0530

    adding output of 2 dices and printing result

--- day40JSBasics.js
+++ day40JSBasics.js
@@ -2,6 +2,15 @@
 /*
 //To Get a Single Digit Number .
 console.log(Math.floor((Math.random()*10)%10));
-*/
+
 //Use Random to get Dice Number between 1 to 6
 console.log(Math.floor((Math.random()*(6)+1)));
+*/
+
+//Add two Random Dice Number and Print the Result
+const dice1Output=Math.floor((Math.random()*(6)+1));
+console.log("Dice 1 output :" +dice1Output);
+const dice2Output=Math.floor((Math.random()*(6)+1));
+console.log("Dice 2 output :" +dice2Output);
+const addResult=parseInt(dice1Output+dice2Output);
+console.log("The sum of " + dice1Output + " and " + dice2Output + " is: " + addResult);
