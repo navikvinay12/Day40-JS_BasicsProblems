eb9c75a113c1e6946e37e0719d360850d1b9be2c
Vinay <navik.vinay12@gmail.com>
2022-12-29 01:36:24 +0530

    conversion inch to feet to meters to acres

--- day40JSBasics.js
+++ day40JSBasics.js
@@ -13,7 +13,6 @@ const dice2Output=Math.floor((Math.random()*(6)+1));
 console.log("Dice 2 output :" +dice2Output);
 const addResult=parseInt(dice1Output+dice2Output);
 console.log("The sum of " + dice1Output + " and " + dice2Output + " is: " + addResult);
-*/
 
 //Write a Program that reads 5 Random 2 Digit values,then find their sum and the average.
 const num1 = Math.floor((Math.random()*(90)+10));
@@ -25,3 +24,15 @@ const sum = num1 + num2 +num3 + num4 + num5;
 console.log("Addition of all given 5 numbers : " +sum);
 const average=sum/5;
 console.log("Average : "+average);
+*/
+
+//Unit Conversion
+const inch =1;
+const feet = inch*12 ;
+console.log("Given inch : 42 ,Converting into Feet :"+(42/feet));
+var mul = 60*40;
+var Plot= mul/3.281;
+console.log("Plot of 60 feet * 40 feet is:"+ Plot+" meters");
+var TwentyFivePlots = Plot*25;
+var inAcres = TwentyFivePlots/4047;
+console.log("Area of 25 Plots in acres :"+ inAcres);
