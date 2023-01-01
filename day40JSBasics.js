0c14e178cd2e5370f7c032be9976433ea076ac1e
navikvinay12 <Navik.vinay12@gmail.com>
2023-01-01 23:53:03 +0530

    added selection -else if problem

--- day40JSBasics.js
+++ day40JSBasics.js
@@ -1,3 +1,4 @@
+const prompt = require("prompt-sync")();        //importing package to use prompt for taking user input .
 //  JavaScript Basic Practice Problems  .
 /*
 //To Get a Single Digit Number .
@@ -24,7 +25,6 @@ const sum = num1 + num2 +num3 + num4 + num5;
 console.log("Addition of all given 5 numbers : " +sum);
 const average=sum/5;
 console.log("Average : "+average);
-*/
 
 //Unit Conversion
 const inch =1;
@@ -36,3 +36,46 @@ console.log("Plot of 60 feet * 40 feet is:"+ Plot+" meters");
 var TwentyFivePlots = Plot*25;
 var inAcres = TwentyFivePlots/4047;
 console.log("Area of 25 Plots in acres :"+ inAcres);
+*/
+
+//-----if else Selection Practice Problems------------
+
+//Program that reads 5 Random 3 digit values and then outputs the min and max value .
+const num1 = Math.floor((Math.random()*(900)+100));
+const num2 = Math.floor((Math.random()*(900)+100));
+const num3 = Math.floor((Math.random()*(900)+100));
+const num4 = Math.floor((Math.random()*(900)+100));
+const num5 = Math.floor((Math.random()*(900)+100));
+console.log("All 5 Three Digit Random Numbers : "+num1 +" "+num2+" " +num3+" " +num4+" " +num5)
+let min=Math.min(num1,num2,num3,num4,num5)
+console.log("Minimum no is :"+min)
+let max=Math.max(num1,num2,num3,num4,num5)
+console.log("Maximum no is :"+max)
+
+//Write a program that takes day and month from the command line and prints true if day of month is between March 20 and June 20, false otherwise.
+var dayInput =prompt("Enter day number");
+var day=parseInt(dayInput);
+var monthInput=prompt("enter month number");
+var month=parseInt(monthInput);
+if(day>=20 && day<=31 && month==3 || day>=1 && day<<30 && month==4 || day>=1 && day<=31 && month==5 || day>=1 && day<=20 && month==6)
+console.log("True")
+else
+console.log("False")
+
+//Leap year or not .
+var yearInput =prompt("Enter day number");
+var year=parseInt(yearInput);
+if(year%4==0)
+console.log("Leap year")
+else
+console.log("not a leap year")
+
+//coin flipped and print out "Heads" or "Tails" accordingly
+const coinFlipped = Math.floor((Math.random()*2));
+if(coinFlipped==0)
+console.log("Heads")
+else
+console.log("Tails")
+
+
+
